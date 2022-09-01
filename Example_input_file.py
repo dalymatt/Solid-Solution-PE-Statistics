28992aeeb08cb34b4eefbbf0ae7db46220db7c16
dalymatt <75578663+dalymatt@users.noreply.github.com>
2022-09-01 16:26:02 -0500

    Update Example_input_file.py
    Updated DOI of paper

--- Example_input_file.py
+++ Example_input_file.py
@@ -3,7 +3,7 @@ Advanced Materials and Microstructures Lab
 University of Illinois at Chicago, 2022
 https://amml.lab.uic.edu/
 
-Please cite: xxxx when using
+Please cite: Jagatramka et al. (2022), Comp. Mater. Sci. 214 111763, doi: 10.1016/j.commatsci.2022.111763Get
 
 """
 #Here, necessary Python packages will be imported and other written scripts will be called to perform the energy statistics calculations herein
